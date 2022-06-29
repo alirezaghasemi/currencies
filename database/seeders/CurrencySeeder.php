25dda94e858a7a4102fe19575d1b7312f6ea0a53
alirezaghasemi <ghasemial@yahoo.com>
2022-06-29 14:15:21 +0430

    write currency seeder

--- database/seeders/CurrencySeeder.php
+++ database/seeders/CurrencySeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Currency;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class CurrencySeeder extends Seeder
@@ -9,6 +11,12 @@ class CurrencySeeder extends Seeder
 
     public function run()
     {
-        //
+        $currencies = [
+            ['name' => 'Bitcoin', 'symbol' => 'btc', 'fee' => '1', 'created_at' => Carbon::now()->format('Y-m-d H:i:s'), 'updated_at' => Carbon::now()->format('Y-m-d H:i:s')],
+            ['name' => 'Ethereum', 'symbol' => 'eth', 'fee' => '2', 'created_at' => Carbon::now()->format('Y-m-d H:i:s'), 'updated_at' => Carbon::now()->format('Y-m-d H:i:s')],
+            ['name' => 'Tron', 'symbol' => 'trx', 'fee' => '10', 'created_at' => Carbon::now()->format('Y-m-d H:i:s'), 'updated_at' => Carbon::now()->format('Y-m-d H:i:s')]
+        ];
+
+        Currency::query()->insert($currencies);
     }
 }
