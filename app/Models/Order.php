9a0c6909b5f05f3d30fccd59627a5d9804228a01
alirezaghasemi <ghasemial@yahoo.com>
2022-06-30 17:31:21 +0430

    write order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Order extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'orders';
+
+    protected $fillable = ['user_id', 'currency_id', 'amount', 'fee'];
+
+    public function users()
+    {
+        return self::hasMany(User::class, 'user_id');
+    }
+
+    public function currency()
+    {
+        return self::hasMany(Currency::class, 'currency_id');
+    }
 }
